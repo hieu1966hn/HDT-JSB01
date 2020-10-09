dc65a467a4063bcc2a5da9cd42cb7aaec84e2336
hieu1966hn <hieu1966hn@gmail.com>
2020-10-09 19:17:43 +0700

    da up code lession 7

---
+++ Lession 7/app.js
@@ -0,0 +1,49 @@
+/// code phần bài 2 ( BTVN lession 6): 
+
+// let n = Number(prompt("nhap vao so nguyen n"));
+// let i = 1;
+// let j = 1;
+// // let gt = n;
+// while (i < n) {
+//     i = i + 1
+//     j = j * i
+// }
+// // console.log(`${n}! = ${j}`);
+// console.log(n + "! = " + j);
+
+
+/// code phần bài tập nhập mảng
+// let n;
+// do {
+//     n = Number(prompt("nhap vao n nguyen duong"));
+// }
+// while (n <= 0 || !(parseInt(n) === n));
+// const a = [];
+// for (let i = 0; i < n; i++) {
+//     a.push(Number(prompt(`nhap vao a[${i}] = `)));
+// }
+// console.log(a); // in ra mảng mình đã nhập
+
+
+
+
+// chữa bài tập phần BMI;
+// let height = Number(prompt("nguoi dung nhap vao chieu cao (cm)"));
+// height = height / 100;
+// let weight = Number(prompt("nguoi dung nhap vao can nang (kg)"));
+// let BMI = weight / (height * height);
+// if (BMI < 16) {
+//     console.log(("you are Severely underweight"));
+// }
+// else if (16 <= BMI < 18.5) {
+//     console.log("you are Underweight");
+// }
+// else if (18.5 <= BMI < 25) {
+//     console.log("you are normal");
+// }
+// else if (25 <= BMI < 30){
+//     console.log("you are Overweight");
+// }
+// else {
+//     console.log("you are Obese");
+// }

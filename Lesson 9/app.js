090ac52870d12c0559c57cd0a6814bd36aa0328f
NGUYEN TRUNG HIEU <hieu1966hn@gmail.com>
2020-10-29 16:02:45 +0700

    hello world

---
+++ Lesson 9/app.js
@@ -0,0 +1,17 @@
+/*
+Thuộc tính value:: Thuộc tính value chỉ định một giá trị khởi tạo cho phần tử input:
+Thuộc tính readonly:Thuộc tính readonly chỉ đinh phần tử input chỉ có thể đọc (không thể thay đổi dữ liệu trên phần tử đó):
+Thuộc tính disabled: Thuộc tính disabled chỉ định trường input sẽ bị ẩn. Phần tử disabled sẽ không được sử dụng và không kích được trên nó. Các phần tử disabled sẽ không được gửi đi (submit).
+Thuộc tính size: Thuộc tính size chỉ định kích thước của trường input (số ký tự):
+Thuộc tính maxlength: Thuộc tính maxlength chỉ định độ dài tối đa cho phép của trường input:
+Thuộc tính autocomplete: gợi ý ...
+Thuộc tính autofocus: load trang sẽ vào @@
+Thuộc tính required: Thuộc tính required là một thuộc tính kiểu boolean. Khi xuất hiện nó xác định rằng trường input không được để trống trước khi submit form
+
+*/
+// let element = document.getElementsByTagName("input");
+// document.write("day la gia tri lay ra tu the input o vi tri thu 0 " + element[1].value);
+
+// let formSubmit = document.getElementById("note").addEventListener("submit", (e) => {
+//     e.preventDefault();
+// })
